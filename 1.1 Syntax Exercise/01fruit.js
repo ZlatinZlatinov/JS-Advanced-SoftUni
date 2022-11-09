a5968e92ef24e21b4c27e500d5feb1100bee0727
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-11-09 21:54:51 +0200

    Delete 1.1 Syntax Exercise directory

--- 1.1 Syntax Exercise/01fruit.js
+++
@@ -1,5 +0,0 @@
-function fruit(fruits, wieghtGrams, cost){
-    let result = (wieghtGrams / 1000) * cost; 
-    console.log(`I need $${result.toFixed(2)} to buy ${(wieghtGrams / 1000).toFixed(2)} kilograms ${fruits}.`);
-} // 100/100 ydb
-fruit('orange', 2500, 1.80); 
