26754c4b8a7336d8dc997d0be077fcdc5b479494
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-02-26 18:29:38 +0200

    file upload

---
+++ 03 Objects and composition/01cityRcord.js
@@ -0,0 +1,10 @@
+function cityRcord(name, population, treasury) {
+    return {
+        'name': name,
+        'population': population,
+        'treasury': treasury
+    }
+} // 100/100
+console.log(cityRcord('Tortuga',
+    7000,
+    15000));
