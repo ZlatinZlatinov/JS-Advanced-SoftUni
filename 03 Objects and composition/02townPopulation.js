6634f1a49a6dd6c9d2b48013b5d64aab5e6b1145
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-02-26 18:29:55 +0200

    file upload

---
+++ 03 Objects and composition/02townPopulation.js
@@ -0,0 +1,26 @@
+function townPopulation(userInput) {
+    let obj = {};
+    for (let line of userInput) {
+        let [city, population] = line.split(' <-> ');
+
+        if (obj.hasOwnProperty(city)) {
+            obj[city] += Number(population);
+        } else {
+            obj[city] = Number(population);
+        }
+    }
+
+    for (let keys in obj) {
+        console.log(`${keys} : ${obj[keys]}`);
+    }
+} // 100/100
+townPopulation(['Sofia <-> 1200000',
+    'Montana <-> 20000',
+    'New York <-> 10000000',
+    'Washington <-> 2345000',
+    'Las Vegas <-> 1000000']);
+townPopulation(['Istanbul <-> 100000',
+    'Honk Kong <-> 2100004',
+    'Jerusalem <-> 2352344',
+    'Mexico City <-> 23401925',
+    'Istanbul <-> 1000']);
