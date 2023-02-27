7f2d0fba8043ce42cad4c9c068df950cd7eec90f
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-02-27 22:52:19 +0200

    file upload

---
+++ 03 Objects and composition/05assemblyLine.js
@@ -0,0 +1,68 @@
+function createAssemblyLine() {
+    let myObject = {
+        hasClima: (inputObject) => {
+            inputObject.temp = 21; //default
+            inputObject.tempSettings = 21; //default 
+            inputObject.adjustTemp = function () {
+                if (this.temp < this.tempSettings) {
+                    this.temp += 1;
+                } else if (this.temp > this.tempSettings) {
+                    this.temp -= 1;
+                }
+            }
+        },
+
+        hasAudio: (inputObject) => {
+            inputObject.currentTrack = null; // default
+            inputObject.nowPlaying = function () {
+                if (this.currentTrack !== null) {
+                    console.log(`Now playing '${this.currentTrack.name}' by ${this.currentTrack.artist}`);
+                }
+            }
+        },
+
+        hasParktronic: (inputObject) => {
+            inputObject.checkDistance = (distance) => {
+                if (distance < 0.1) {
+                    console.log('Beep! Beep! Beep!');
+                } else if (distance >= 0.1 && distance < 0.25) {
+                    console.log('Beep! Beep!');
+                } else if (distance >= 0.25 && distance < 0.5) {
+                    console.log('Beep!');
+                } else {
+                    console.log('');
+                }
+            }
+        }
+
+    }
+    return myObject;
+} // 100/100 ... now playing neva gonna give u up 
+// lambda functions dont work with 'this.something' !!!!
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2); //- seems to work?? 
+
+console.log(myCar);
+
+
